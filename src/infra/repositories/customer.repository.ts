2b0fdddd8beca1f3a200dc858ac3f0322ae4d032
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-07-06 17:18:16 -0300

    fix: adjust CustomerRepository definition

--- src/infra/repositories/customer.repository.ts
+++ src/infra/repositories/customer.repository.ts
@@ -1,12 +1,12 @@
-import { TransactionHistory } from "../entities/transaction/transaction-history.entity";
+import { Customer } from "../entities/customer/customer.entity";
 import { dataSource } from "../database/datasource.config";
 
 import { Repository } from "typeorm";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class TransactionHistoryRepository extends Repository<TransactionHistory> {
+export class CustomerRepository extends Repository<Customer> {
   constructor() {
-    super(TransactionHistory, dataSource.createEntityManager());
+    super(Customer, dataSource.createEntityManager());
   }
 }
