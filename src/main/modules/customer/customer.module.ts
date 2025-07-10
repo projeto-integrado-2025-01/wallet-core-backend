3896361024c757bddd227879669ac0b466011925
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-07-10 10:30:53 -0300

    feat: register components in module

--- src/main/modules/customer/customer.module.ts
+++ src/main/modules/customer/customer.module.ts
@@ -1,19 +1,24 @@
-import { WalletRepository } from 'src/infra/repositories/wallet.repository';
+import { GetCustomerByIdController } from 'src/application/controllers/customer/get-customer-by-id.controller';
 import { CreateCustomerController } from 'src/application/controllers/customer/create-customer.controller';
+import { GetCustomerByIdService } from 'src/domain/customer/use-cases/get-customer-by-id.service';
 import { CreateCustomerService } from 'src/domain/customer/use-cases/create-customer.service';
 import { CustomerRepository } from 'src/infra/repositories/customer.repository';
+import { WalletRepository } from 'src/infra/repositories/wallet.repository';
+import { DataFormater } from 'src/infra/gateways/dataFormater';
 
 import { Module } from '@nestjs/common';
-import { DataFormater } from 'src/infra/gateways/dataFormater';
+import { HashProvider } from 'src/infra/gateways/hash-provider';
 
 @Module({
   imports: [],
-  controllers: [CreateCustomerController],
+  controllers: [CreateCustomerController, GetCustomerByIdController],
   providers: [
     CreateCustomerService,
+    GetCustomerByIdService,
     WalletRepository,
     CustomerRepository,
     DataFormater,
+    HashProvider,
   ],
 })
 export class CustomerModule {}
